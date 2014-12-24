7b4951b3a6ee9cd43700a0c22728f39c992dd7f8
Rafael Portela <rafaelportela90@gmail.com>
2014-12-24 13:34:25 +0200

    All-todos route.

--- src/clojure_todos/core/handler.clj
+++ src/clojure_todos/core/handler.clj
@@ -1,11 +1,21 @@
 (ns clojure-todos.core.handler
+  (:use ring.util.response)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.json :as middleware]
+            [compojure.handler :as handler]))
+
+(defn all-todos []
+  (response
+    [{:id 123 :title "nice title"}]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/todos" [] (all-todos))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> (handler/api app-routes)
+    (middleware/wrap-json-body)
+    (middleware/wrap-json-response)))
